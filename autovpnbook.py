676c4170e8a81f2c9b62e0738ecd18c3ef60ae3c
Rupert Edwards <ruped24@gmail.com>
2017-04-14 20:55:41 +0000

    autovpnbook.py edited online with Bitbucket

--- autovpnbook.py
+++ autovpnbook.py
@@ -12,27 +12,31 @@ from urllib2 import urlopen
 import pexpect
 
 
-def get_password():
-  with closing(urlopen('http://www.vpnbook.com/#openvpn')) as request:
-    html = request.read().split()
-    passwd = html.index('<li><strong>Password:') + 1
-    Password = html[passwd].split('<')[0]
-  return Password
+class AutoVpnBook(object):
 
+  def __init__(self):
+    self.Password = self.get_password()
 
-def auto_login(ovpn):
-  openvpnbook = pexpect.spawn('openvpn --config %s' % ''.join(ovpn))
-  openvpnbook.expect('Enter Auth Username:')
-  openvpnbook.sendline('vpnbook')
-  openvpnbook.expect('Enter Auth Password:')
-  openvpnbook.sendline(get_password())
-  openvpnbook.interact()
+  def get_password(self):
+    with closing(urlopen('http://www.vpnbook.com/#openvpn')) as request:
+      html = request.read().split()
+      passwd = html.index('<li><strong>Password:') + 1
+      Password = html[passwd].split('<')[0]
+    return Password
+
+  def auto_login(self, ovpn):
+    openvpnbook = pexpect.spawn('openvpn --config %s' % ''.join(ovpn))
+    openvpnbook.expect('Enter Auth Username:')
+    openvpnbook.sendline('vpnbook')
+    openvpnbook.expect('Enter Auth Password:')
+    openvpnbook.sendline(self.Password)
+    openvpnbook.interact()
 
 
 if __name__ == '__main__':
   if geteuid() is not 0:
     exit("\033[91m[!]\033[0m Run as super user!")
   try:
-    auto_login(argv[1:])
+    AutoVpnBook().auto_login(argv[1:])
   except:
     exit("\nUsage: %s <vpnbook-file-proto.ovpn> \n" % basename(__file__))
